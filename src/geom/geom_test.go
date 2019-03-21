7a3d0c8ea3a079f8c61aa2a6e75f39be5a24d355
Jozef Reisinger <jozef.reisinger@telekom.com>
2019-03-21 12:59:15 +0100

    improve tests

--- src/geom/geom_test.go
+++ src/geom/geom_test.go
@@ -6,16 +6,25 @@ import (
 	"testing"
 )
 
-func TestCircleArea(t *testing.T) {
-	c := &geom.Circle{0, 0, 5}
-	v := fmt.Sprintf("%.2f", c.Area())
-	if v != "78.54" {
-		t.Error("Expected 78.54, got ", v)
-	}
+type testpair struct {
+	input    []float64
+	expected string
+}
 
-	c = &geom.Circle{0, 0, 0}
-	v = fmt.Sprintf("%.2f", c.Area())
-	if v != "0.00" {
-		t.Error("Expected 0.00, got ", v)
+var tests = []testpair{
+	{[]float64{0, 0, 0}, "0.00"},
+	{[]float64{0, 0, 5}, "78.54"},
+	{[]float64{1, 0, 5}, "78.54"},
+	{[]float64{0, 1, 5}, "78.54"},
+	{[]float64{1, 1, 5}, "78.54"},
+}
+
+func TestCircleArea(t *testing.T) {
+	for _, pair := range tests {
+		c := &geom.Circle{pair.input[0], pair.input[1], pair.input[2]}
+		out := fmt.Sprintf("%.2f", c.Area())
+		if out != pair.expected {
+			t.Error("Expected 0.00, got ", out)
+		}
 	}
 }
